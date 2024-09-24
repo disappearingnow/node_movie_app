ccef1d0ea20ed214cd2071c4aab7ad6ae3741044
Connor <disappearing.now@gmail.com>
2024-09-23 22:13:39 -0400

    Formatting

--- frontend/src/components/atoms/MovieCard/MovieCard.tsx
+++ frontend/src/components/atoms/MovieCard/MovieCard.tsx
@@ -1,9 +1,19 @@
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
-export default function MovieCard({ title, year, posterUrl }: { title: string, year: string, posterUrl: string }) {
-    return <div className={styles.moviecard}>
-        <img src={posterUrl} alt={`${title} poster`} />
-        <p className={styles.title}>{title}</p>
-        <p className={styles.year}>{year}</p>
+export default function MovieCard({
+  title,
+  year,
+  posterUrl,
+}: {
+  title: string;
+  year: string;
+  posterUrl: string;
+}) {
+  return (
+    <div className={styles.moviecard}>
+      <img src={posterUrl} alt={`${title} poster`} />
+      <p className={styles.title}>{title}</p>
+      <p className={styles.year}>{year}</p>
     </div>
+  );
 }
