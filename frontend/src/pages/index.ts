91615a516b9e3c6d811610aa2ff332d7384de163
Connor <disappearing.now@gmail.com>
2024-09-24 13:07:13 -0400

    Setting up pages

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,6 @@
+import DashboardPage from "./DashboardPage/DashboardPage";
+import LandingPage from "./Landing/LandingPage";
+import LoginPage from "./Login/LoginPage";
+import RegisterPage from "./Register/RegisterPage";
+
+export { DashboardPage, LandingPage, LoginPage, RegisterPage };
