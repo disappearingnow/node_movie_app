06985064b3189e5f419144da59ce3ef992a2f788
Connor <disappearing.now@gmail.com>
2024-09-23 23:44:24 -0400

    Adding login and register paths

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <div>Home page</div>
-    }
-])
+  {
+    path: "/",
+    element: <div>Home page</div>,
+  },
+  {
+    path: "/login",
+    element: <div>Login</div>,
+  },
+  {
+    path: "/register",
+    element: <div>Register</div>,
+  },
+]);
 
 export default router;
