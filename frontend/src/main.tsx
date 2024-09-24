a95467b1ff4471587d839e6af3fa9582ab6652ab
Connor <disappearing.now@gmail.com>
2024-09-24 16:02:44 -0400

    Moving RouterProvider to App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom';
-import router from './router.tsx';
-import './reset.css'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./reset.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
