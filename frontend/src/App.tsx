74fa1ff0193c53d390b7db05d992597f5e16b237
Connor <disappearing.now@gmail.com>
2024-09-24 16:02:23 -0400

    Moving RouterProvider to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import { RegisterForm } from "./components/molecules";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import { Navbar } from "./components/molecules";
 import "./App.css";
 
 export default function App() {
   return (
     <>
-      <RegisterForm />
+      <Navbar />
+      <RouterProvider router={router} />
     </>
   );
 }
-
