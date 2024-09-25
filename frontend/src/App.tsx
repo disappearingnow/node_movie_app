8e64a991e1f9b1905cfaa32f44d4553da531714a
Connor <disappearing.now@gmail.com>
2024-09-25 17:38:43 -0400

    Moving reset.css import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { Navbar } from "./components/molecules";
+import "./reset.css";
 import "./App.css";
 
 export default function App() {
