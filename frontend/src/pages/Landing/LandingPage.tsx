0fce50205e2c1138ad4b603429781d11a3baa369
Connor <disappearing.now@gmail.com>
2024-09-26 15:26:55 -0400

    Changing landing page message

--- frontend/src/pages/Landing/LandingPage.tsx
+++ frontend/src/pages/Landing/LandingPage.tsx
@@ -1,4 +1,9 @@
+import styles from "./styles.module.css";
+
 export default function LandingPage() {
-    return <div>Landing Page</div>;
-  }
-  
+  return (
+    <div className={styles.pageContainer}>
+      <h1>Please log in or register to use the app :D</h1>
+    </div>
+  );
+}
