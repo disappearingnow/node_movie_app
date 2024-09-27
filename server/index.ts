3bceace416b215fd2df7cff99c1db1b5df8eb1c9
Connor <disappearing.now@gmail.com>
2024-09-27 17:15:56 -0400

    Starting code to handle login requests

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,38 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { authRouter } from "./routes";
+import users from "./fakeUserData";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cors());
 app.use("/auth", authRouter);
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("Welcome");
+  return res.status(200).send("Welcome");
 });
 
 app.post("/login", (req: Request, res: Response) => {
-  res.send(req.body);
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send("You need to provide a username and password");
+  }
+
+  const { username, password } = req.body;
+
+  const doesUserExist = users.filter(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (doesUserExist.length === 0) {
+    return res.status(400).send("User does not exist");
+  }
+
+  return res.status(200).send(`User ${username} does exist`);
 });
 
 app.listen(port, () => {
