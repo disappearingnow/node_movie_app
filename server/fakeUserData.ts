fe2abe8c9f5287be539f11a186e5405f77a44bdb
Connor <disappearing.now@gmail.com>
2024-09-27 17:14:45 -0400

    Updating user data

--- server/fakeUserData.ts
+++ server/fakeUserData.ts
@@ -1,14 +1,19 @@
 const users = [
-    {
-        id:1,
-        username: "joe"
-    },
-    {
-        id:2,
-        username: "matt"
-    },
-    {
-        id:3,
-        username: "lily"
-    }
-]
+  {
+    id: 1,
+    username: "joe123",
+    password: "joespassword",
+  },
+  {
+    id: 2,
+    username: "matt123",
+    password: "mattspassword",
+  },
+  {
+    id: 3,
+    username: "lily123",
+    password: "lilyspassword",
+  },
+];
+
+export default users;
