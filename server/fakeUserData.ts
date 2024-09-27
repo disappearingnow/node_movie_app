1a69228cdaa56a31c63f75e4ffbdf58c3b9522f1
Connor <disappearing.now@gmail.com>
2024-09-27 15:42:30 -0400

    Creating fake user data

---
+++ server/fakeUserData.ts
@@ -0,0 +1,14 @@
+const users = [
+    {
+        id:1,
+        username: "joe"
+    },
+    {
+        id:2,
+        username: "matt"
+    },
+    {
+        id:3,
+        username: "lily"
+    }
+]
