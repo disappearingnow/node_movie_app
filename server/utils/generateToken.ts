fe95b33a3136deccd4758cea87c04021058e52bd
Connor <disappearing.now@gmail.com>
2024-09-27 15:45:02 -0400

    Converted to function declaration

--- server/utils/generateToken.ts
+++ server/utils/generateToken.ts
@@ -1,10 +1,9 @@
 import * as jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
-dotenv.config({path:'../.env'});
+dotenv.config({ path: "../.env" });
 const secret = process.env.JWT_SECRET!;
 
-const generateToken = (userId: number): string => {
+export default function generateToken(userId: number): string {
   return jwt.sign({ userId }, secret, { expiresIn: "1h" }); // Token expires in 1 hour
-};
- 
+}
